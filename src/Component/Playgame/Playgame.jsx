667687ed59daf54c04f0408a3c81a100af9bd9c9
Rupam <rupammondal564@gmail.com>
2024-08-04 20:10:37 +0530

    Rule box and word rendering bug fixed

--- src/Component/Playgame/Playgame.jsx
+++ src/Component/Playgame/Playgame.jsx
@@ -1,18 +1,33 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Randomwords } from "../Randomwords/Randomwords"
 import MaskedText from "../MaskedText/MaskedText"
 import Keyboardkeys from "../Keyboardkeys/Keyboardkey"
 import './hint.css'
+import { RuleBox } from "../Rulebox/RuleBox"
 
 function Playgame(){
     const [randomobj, setObj] = useState(Randomwords)
     const [usedLetters, setLetters] = useState([])
+    const objectword = randomobj.word.toUpperCase()
+    const [winningString, setWinningString] = useState("");
+    useEffect(() => {
+        console.log(winningString)
+    } , [winningString])
 
     function handleclick(e) {
-        console.log(randomobj.word)
-        console.log(randomobj.word.includes(e))
-        console.log(e)
+        if(objectword.includes(e)){
+            setWinningString(prev => prev + e);
+            console.log("Correct")
+        }
+        else{
+            console.log("Incorrect")
+        }
         setLetters([...usedLetters, e])
+    }
+
+    function resetHandler(){
+        setObj(Randomwords)
+        setLetters([])
     }
     return(
         <>
@@ -25,6 +40,8 @@ function Playgame(){
             <Keyboardkeys
                 onclickHandler={handleclick}
             />
+            <button className="resetBtn" onClick={resetHandler}>Reset</button>
+            <RuleBox/>
         </>
     )
 }
---
+++ src/Component/Rulebox/RuleBox.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './Rule.css'
+
+export function RuleBox(){
+    const [display , setDisplay] = useState('block')
+    function onclickHandler(){
+        setDisplay('none')
+    }
+    return(
+        <>
+            <div className="Parent" style={{ display }}>
+                <div className='rulebox'>
+                    <h1>Rules</h1>
+                    <div className='note-txt'>Welcome to the Hangman Game! The objective of this game is to guess a hidden word based on the provided hint. At the beginning of the game, a random word and its corresponding hint are selected. The word is displayed as a series of underscores, with each underscore representing a letter in the word.
+                        click Start button below to start the game
+                    </div>
+                    <button className='str-btn' onClick={onclickHandler}>Start Game</button>
+                </div>
+            </div>
+        </>
+    )
+}
